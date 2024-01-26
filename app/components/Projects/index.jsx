46bf38e2260661d944c04111a8c6a0aac4ce7e8c
ednavarrol <carthagoxz@gmail.com>
2024-01-25 22:31:08 -0300

    Fixed positioning for project modal

--- app/components/Projects/index.jsx
+++ app/components/Projects/index.jsx
@@ -9,9 +9,9 @@ import { projects } from '@/app/data';
 import Project from "./Project";
 
 const scaleAnimation = {
-    initial: {scale: 0, x: "50%", y: "50%"},
-    enter: {scale: 1, x: "50%", y: "50%", transition: {duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
-    closed: {scale: 0, x: "50%",y: "50%", transition: {duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
+    initial: {scale: 0, x: "-50%", y: "-50%"},
+    enter: {scale: 1, x: "-50%", y: "-50%", transition: {duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
+    closed: {scale: 0, x: "-50%",y: "-50%", transition: {duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
 }
 
 export default function index() {

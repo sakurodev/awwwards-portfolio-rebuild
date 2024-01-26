80d5078ee5e60195e3cd2018b823f0aabd9c435d
ednavarrol <carthagoxz@gmail.com>
2024-01-25 22:28:30 -0300

    Removed imports no longer needed

--- app/page.js
+++ app/page.js
@@ -1,16 +1,10 @@
 'use client' 
 import Image from 'next/image';
 import styles from './page.module.css';
-import { useRef, useEffect, useState } from 'react';
-import gsap from 'gsap';
-import { projects } from './data';
 import Projects from './components/Projects';
 import Marquee from './components/Marquee';
-import Modal from './components/Modal';
 
 export default function Home() {
-
-  const [modal, setModal] = useState({active: false, index: 0})
 
   return (
     <main>
